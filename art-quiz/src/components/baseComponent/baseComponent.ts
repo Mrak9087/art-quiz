4f1adcd8ee4ae8168bcab825adb00fe640d2e42a
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2022-01-02 22:12:01 +0400

    fix: add helpers to baseComponent

--- art-quiz/src/components/baseComponent/baseComponent.ts
+++ art-quiz/src/components/baseComponent/baseComponent.ts
@@ -1,8 +1,9 @@
+import { createHTMLElement } from '../helpers/helpers';
+
 export class BaseComponent {
     readonly node: HTMLElement;
 
     constructor(className: string, tagName: keyof HTMLElementTagNameMap = 'div') {
-        this.node = document.createElement(tagName);
-        this.node.className = className;
+        this.node = createHTMLElement(tagName, className);
     }
 }
